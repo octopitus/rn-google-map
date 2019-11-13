84d6e73e805a9131a91c008bf6bb6edae88907ec
Luy Tran <octopitus@gmail.com>
2019-11-13 17:01:17 +0800

    Update React Native to 0.61

--- index.js
+++ index.js
@@ -7,5 +7,9 @@ import {AppRegistry, YellowBox} from 'react-native'
 import App from './App'
 import {name as appName} from './app.json'
 
-YellowBox.ignoreWarnings(['Warning: component'])
+YellowBox.ignoreWarnings([
+  'Warning: componentWillMount',
+  'Warning: `-[RCTRootView cancelTouches]`'
+])
+
 AppRegistry.registerComponent(appName, () => App)
